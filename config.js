8c67a562afd0fd390879ebfe5a1cdc66fca8733c
dophuc0111001 <100935352+dophuc0111001@users.noreply.github.com>
2022-04-18 07:49:02 +0700

    Add files via upload

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Châu Phúc';
+const giftUrl = '';
+const eventName = 'Chúc Mừng Sinh Nhật';
+const titleCard = 'Tặng bạn tôi';
+const contentCard = 'Thay mặt Chủ Tịch nước, Chủ tịch Quốc hội, các bộ trưởng, hơn 9 triệu dân Việt Nam cùng hơn 7 tỷ dân trên thế giới, chúc mừng sinh nhật con bạn của tôi nhé!';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'nchauphuc.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
